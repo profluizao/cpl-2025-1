b49faba2799bd318f6cdb9b1c70cdb0a8d22a70e
profluizao <prof.luiz.a.rodrigues.2018@gmail.com>
2025-04-23 20:28:39 -0400

    Encontro 10 - Projeto LexClipper

---
+++ ProjetoLexClipper/LexClipper/src/App.java
@@ -0,0 +1,9 @@
+import Exemplos.MapearIdades;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //ExemploHashMap.executar();
+        MapearIdades mapa = new MapearIdades();
+        mapa.executar();
+    }
+}
